2d6ca7c4053dc119e2fd2cafe23cb553adad81b3
Kristin1 <christina.manukyan@sfl.am>
2021-07-27 15:44:21 +0400

    first

---
+++ src/main/java/com/example/myapp/App.java
@@ -0,0 +1,167 @@
+package com.amazonaws.codesamples;
+
+import test.SdkClientException;
+//import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.SigningAlgorithm;
+import com.amazonaws.util.BinaryUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static com.amazonaws.auth.internal.SignerConstants.AWS4_TERMINATOR;
+import static com.amazonaws.util.StringUtils.UTF8;
+
+public class App {
+
+    public static String httpMethod = "GET";
+    public static String action = "connect";
+    public static String canonicalURIParameter = "/";
+    public static SortedMap<String, String> canonicalQueryParameters = new TreeMap();
+    public static String payload = StringUtils.EMPTY;
+    public static String signedHeader = "host";
+    public static String algorithm = "AWS4-HMAC-SHA256";
+    public static String serviceName = "rds-db";
+    public static String requestWithoutSignature;
+
+    public static void main(String[] args) throws Exception {
+
+        String region = "us-east-1";
+        String instanceName = "anthill-staging-rds.c4tmpumuvt87.us-east-1.rds.amazonaws.com";
+        String port = "5432";
+        String username = "postgres";
+	
+        Date now = new Date();
+        String date = new SimpleDateFormat("yyyyMMdd").format(now);
+        String dateTimeStamp = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'").format(now);
+        DefaultAWSCredentialsProviderChain creds = new DefaultAWSCredentialsProviderChain();
+	    String awsAccessKey = creds.getCredentials().getAWSAccessKeyId();
+	    String awsSecretKey = creds.getCredentials().getAWSSecretKey();
+        String expiryMinutes = "-1";
+        
+        System.out.println("Step 1:  Create a canonical request:");
+        String canonicalString = createCanonicalString(username, awsAccessKey, date, dateTimeStamp, region, expiryMinutes, instanceName, port);
+        System.out.println(canonicalString);
+        System.out.println();
+
+        System.out.println("Step 2:  Create a string to sign:");        
+        String stringToSign = createStringToSign(dateTimeStamp, canonicalString, awsAccessKey, date, region);
+        System.out.println(stringToSign);
+        System.out.println();
+
+        System.out.println("Step 3:  Calculate the signature:");        
+        String signature = BinaryUtils.toHex(calculateSignature(stringToSign, newSigningKey(awsSecretKey, date, region, serviceName)));
+        System.out.println(signature);
+        System.out.println();
+
+        System.out.println("Step 4:  Add the signing info to the request");                
+        System.out.println(appendSignature(signature));
+        System.out.println();
+        
+    }
+
+    //Step 1: Create a canonical request date should be in format YYYYMMDD and dateTime should be in format YYYYMMDDTHHMMSSZ
+    public static String createCanonicalString(String user, String accessKey, String date, String dateTime, String region, String expiryPeriod, String hostName, String port) throws Exception {
+        canonicalQueryParameters.put("Action", action);
+        canonicalQueryParameters.put("DBUser", user);
+        canonicalQueryParameters.put("X-Amz-Algorithm", "AWS4-HMAC-SHA256");
+        canonicalQueryParameters.put("X-Amz-Credential", accessKey + "%2F" + date + "%2F" + region + "%2F" + serviceName + "%2Faws4_request");
+        canonicalQueryParameters.put("X-Amz-Date", dateTime);
+        canonicalQueryParameters.put("X-Amz-Expires", expiryPeriod);
+        canonicalQueryParameters.put("X-Amz-SignedHeaders", signedHeader);
+        String canonicalQueryString = "";
+        while(!canonicalQueryParameters.isEmpty()) {
+            String currentQueryParameter = canonicalQueryParameters.firstKey();
+            String currentQueryParameterValue = canonicalQueryParameters.remove(currentQueryParameter);
+            canonicalQueryString = canonicalQueryString + currentQueryParameter + "=" + currentQueryParameterValue;
+            if (!currentQueryParameter.equals("X-Amz-SignedHeaders")) {
+                canonicalQueryString += "&";
+            }
+        }
+        String canonicalHeaders = "host:" + hostName + ":" + port + '\n';
+        requestWithoutSignature = hostName + ":" + port + "/?" + canonicalQueryString;
+
+        String hashedPayload = BinaryUtils.toHex(hash(payload));
+        return httpMethod + '\n' + canonicalURIParameter + '\n' + canonicalQueryString + '\n' + canonicalHeaders + '\n' + signedHeader + '\n' + hashedPayload;
+
+    }
+
+    //Step 2: Create a string to sign using sig v4
+    public static String createStringToSign(String dateTime, String canonicalRequest, String accessKey, String date, String region) throws Exception {
+        String credentialScope = date + "/" + region + "/" + serviceName + "/aws4_request";
+        return algorithm + '\n' + dateTime + '\n' + credentialScope + '\n' + BinaryUtils.toHex(hash(canonicalRequest));
+
+    }
+
+    //Step 3: Calculate signature
+    /**
+     * Step 3 of the &AWS; Signature version 4 calculation. It involves deriving
+     * the signing key and computing the signature. Refer to
+     * http://docs.aws.amazon
+     * .com/general/latest/gr/sigv4-calculate-signature.html
+     */
+    public static byte[] calculateSignature(String stringToSign,
+                                            byte[] signingKey) {
+        return sign(stringToSign.getBytes(Charset.forName("UTF-8")), signingKey,
+                SigningAlgorithm.HmacSHA256);
+    }
+
+    public static byte[] sign(byte[] data, byte[] key,
+                          SigningAlgorithm algorithm) throws SdkClientException {
+        try {
+            Mac mac = algorithm.getMac();
+            mac.init(new SecretKeySpec(key, algorithm.toString()));
+            return mac.doFinal(data);
+        } catch (Exception e) {
+            throw new SdkClientException(
+                    "Unable to calculate a request signature: "
+                            + e.getMessage(), e);
+        }
+    }
+
+    public static byte[] newSigningKey(String secretKey,
+                                   String dateStamp, String regionName, String serviceName) {
+        byte[] kSecret = ("AWS4" + secretKey).getBytes(Charset.forName("UTF-8"));
+        byte[] kDate = sign(dateStamp, kSecret, SigningAlgorithm.HmacSHA256);
+        byte[] kRegion = sign(regionName, kDate, SigningAlgorithm.HmacSHA256);
+        byte[] kService = sign(serviceName, kRegion,
+                SigningAlgorithm.HmacSHA256);
+        return sign(AWS4_TERMINATOR, kService, SigningAlgorithm.HmacSHA256);
+    }
+
+    public static byte[] sign(String stringData, byte[] key,
+                       SigningAlgorithm algorithm) throws SdkClientException {
+        try {
+            byte[] data = stringData.getBytes(UTF8);
+            return sign(data, key, algorithm);
+        } catch (Exception e) {
+            throw new SdkClientException(
+                    "Unable to calculate a request signature: "
+                            + e.getMessage(), e);
+        }
+    }
+
+    //Step 4: append the signature
+    public static String appendSignature(String signature) {
+        return requestWithoutSignature + "&X-Amz-Signature=" + signature;
+    }
+
+    public static byte[] hash(String s) throws Exception {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(s.getBytes(UTF8));
+            return md.digest();
+        } catch (Exception e) {
+            throw new SdkClientException(
+                    "Unable to compute hash while signing request: "
+                            + e.getMessage(), e);
+        }
+    }
+}
